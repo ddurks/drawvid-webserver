c2d56c244249c12968ca052b72e49c5e272c6d84
David Durkin <dldurks@gmail.com>
2020-01-12 23:11:00 -0600

    archives and about page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var pg = require('pg');
-var conString = "postgres://" + dbConfig.user + ":" + dbConfig.password + "@localhost:" + dbConfig.port + "/" + dbConfig.database;
+var constants = require('../constants');
+var conString = "postgres://" + constants.dbConfig.user + ":" + constants.dbConfig.password + "@localhost:" + constants.dbConfig.port + "/" + constants.dbConfig.database;
 
 var client = new pg.Client(conString);
 client.connect();
@@ -16,12 +17,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'drawvid.com' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'drawvid.com: about' });
+});
+
+/* GET archives page. */
+router.get('/archives', function(req, res, next) {
+  res.render('archives', { title: 'drawvid.com: archives' });
+});
+
+/* GET gallery page. */
+router.get('/gallery', function(req, res, next) {
+  res.render('gallery', { title: 'drawvid.com: gallery' });
+});
+
 router.get('/fetchpost', function(req, res, next){
   var post = 'latest';
   if (req.query.post) {
     post = req.query.post
   }
-  client.query('SELECT * FROM posts ORDER BY id DESC LIMIT 1')
+  client.query('SELECT * FROM posts ORDER BY id DESC')
   .then(data => {
     var drawing = data.rows[0];
     var index = drawing.id;
@@ -32,6 +48,9 @@ router.get('/fetchpost', function(req, res, next){
       case 'random':
         index = getRandomIndex(drawing.id);
         break;
+      case 'all':
+        res.status(200).send(data.rows);
+        return;
       default:
         index = post;
     }
--- upload/upload.js
+++ upload/upload.js
@@ -1,13 +1,27 @@
 var fs = require('fs');
 var pg = require('pg');
-var dbConfig = require('../constants')
-var conString = "postgres://" + dbConfig.user + ":" + dbConfig.password + "@" + dbConfig.host + ":" + dbConfig.port + "/" + dbConfig.database;
+var SftpUpload = require('sftp-upload');
+var constants = require('../constants');
+var conString = "postgres://" + constants.dbConfig.user + ":" + constants.dbConfig.password + "@" + constants.dbConfig.host + ":" + constants.dbConfig.port + "/" + constants.dbConfig.database;
 var client = new pg.Client(conString);
 client.connect();
 
+var sftp = new SftpUpload(constants.sftpConfig);
+
+sftp.on('error', function(err) {
+  console.log(err);
+})
+.on('uploading', function(progress) {
+  console.log('Uploading', progress.file);
+  console.log(progress.percent+'% completed');
+})
+  .on('completed', function() {
+  console.log('Upload Completed');
+}).upload();
+
 var directoryPath = process.argv[2];
 if (directoryPath == null) {
-  directoryPath = "../posts/"
+  directoryPath = "/Users/dldurks/Desktop/drawings/pngs/"
 }
 var itemsProcessed = 0;
 
@@ -26,15 +40,19 @@ files = files.map(function (fileName) {
 function loadDB(num, file, stats) {
   const text = 'INSERT INTO posts(id, image_name, created_date) VALUES($1, $2, to_timestamp($3 / 1000.0)) RETURNING *'
   const values = [num, file, stats.birthtime.getTime()];
+  var itemsUploaded = 0;
   client.query(text, values, (err, res) => {
     if (err) {
       console.log(err.stack)
     } else {
+      itemsUploaded++;
       console.log(res.rows[0])
-      itemsProcessed++;
-      if(itemsProcessed === files.length) {
-        client.end();
-      }
+    }
+    itemsProcessed++;
+    if(itemsProcessed >= files.length) {
+      console.log('processed ' + itemsProcessed + ' files');
+      console.log('uploaded ' + itemsUploaded + ' new posts to the DB');
+      client.end();
     }
   });
 }
