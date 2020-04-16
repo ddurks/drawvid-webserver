471743e4beee71e484dae9b0ab1ebca42d0eb168
David Durkin <dldurks@gmail.com>
2020-04-16 15:25:08 -0500

    1.1

--- routes/socketApi.js
+++ routes/socketApi.js
@@ -9,13 +9,17 @@ const CANVAS_WIDTH = 512;
 const CANVAS_HEIGHT = 512;
 const PLAYER_DIMENSION = 32;
 const PLAYER_SPEED = 6;
+const BULLET_DIMENSION = 16;
+const BULLET_SPEED = 250;
+const RELOAD_INTERVAL = 60;
 const LOUNGE_CAPACITY = 10;
-const MESSAGE_DECAY = 30.0;
+const MESSAGE_DECAY = 20.0;
 const Utility = {
   getTimestamp: function() {
     return Date.now() / 1000.0;
   }
 }
+
 const Furniture = {
   create: function(image_src, posx, posy, isBlocking) {
     var furniture = Object.create(this);
@@ -52,17 +56,123 @@ const Player = {
     player.direction = direction;
     player.speed = PLAYER_SPEED;
     player.message_timestamp = 0;
+    player.score = 0;
+    player.bullets = 0;
+    player.reload_timestamp = 0;
     return player;
   },
 };
 
+const Bullet = {
+  create: function(x, y, direction) {
+    var bullet = Object.create(this);
+    bullet.x = x;
+    bullet.y = y;
+    bullet.direction = direction;
+    bullet.speed = BULLET_SPEED;
+    return bullet;
+  }
+};
+
+const Bong = {
+  create: function(x, y) {
+    var bong = Object.create(this);
+    bong.x = x;
+    bong.y = y;
+    return bong;
+  }
+};
+
+const SmokePuff = {
+  create: function(x, y) {
+    var smoke = Object.create(this);
+    smoke.x = x;
+    smoke.y = y;
+    smoke.loop = [0,0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1, 2,2,2,2,2,2,2,2,2,2, 3,3,3,3,3,3,3,3,3,3, 4,4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5,5];
+    smoke.loop_i = 0;
+    return smoke;
+  }
+}
+
+const ScoreBoard = {
+  create: function() {
+    var scoreboard = Object.create(this);
+    scoreboard.scores = new Array();
+    scoreboard.scoresMap = new Map();
+    scoreboard.updated = false;
+    return scoreboard;
+  },
+
+  minScore: function() {
+    if (this.scores.length >= 5) {
+      return this.scores[0].score;
+    } else {
+      return 0;
+    }
+  },
+
+  setScoresFromMap: function() {
+    var i = 0;
+    this.scoresMap.forEach( function(score) {
+      this.scores[i] = score;
+      i++;
+    }.bind(this));
+    this.scores.sort( function(a, b) { return a.score-b.score; });
+    this.updated = true;
+  },
+
+  removeLowestScore: function() {
+    var lowestScore = 1000000;
+    var lowestUsername;
+    this.scoresMap.forEach(function(score) {
+      if (score.score < lowestScore) {
+        lowestScore = score.score;
+        lowestUsername = score.name;
+      }
+    });
+    this.scoresMap.delete(lowestUsername);
+  },
+
+  updateScoreboard: function(player) {
+    this.updated = false;
+    if (player.username != "anonymous" && player.username != null && player.username != "") {
+      if (parseInt(player.score, 10) > this.minScore() && !this.scoresMap.has(player.username)) {
+        if (this.scores.length >= 5) {
+          this.removeLowestScore();
+        }
+        var newScore = Score.create(player);
+        this.scoresMap.set(player.username, newScore);
+        this.setScoresFromMap();
+      } else if (this.scoresMap.has(player.username)) {
+        var scoreboardScore = this.scoresMap.get(player.username);
+        if (player.score > scoreboardScore.score) {
+          var newScore = Score.create(player);
+          this.scoresMap.set(player.username, newScore);
+          this.setScoresFromMap();
+        }
+      }
+    }
+  }
+};
+
+const Score = {
+  create: function(player) {
+    var score = Object.create(this);
+    score.name = player.username;
+    score.score = parseInt(player.score, 10);
+    return score;
+  }
+}
+
 const Lounge = {
   players: new Map(),
   furnitureList: new Array(),
+  scoreboard: ScoreBoard.create(),
 
   create: function() {
     var lounge = Object.create(this);
     lounge.addFurniture();
+    lounge.bullets = new Array();
     return lounge;
   },
 
@@ -101,7 +211,89 @@ const Lounge = {
     this.furnitureList.push(right_dinner_chairs);
     var bottom_dinner_chair = Furniture.create("onlinelounge/assets/bottom_dinner_chair.png", 128, 481, false);
     this.furnitureList.push(bottom_dinner_chair);
-  }
+    var bong = Bong.create(280, 117);
+    this.bong = bong;
+  },
+
+  update_all_players: function(deltat) {
+    this.players.forEach( function(player) {
+      this.update_player(player, deltat);
+    }.bind(this));
+    if (this.players.size > 0) {
+      io.sockets.emit('state', Array.from(this.players.values()));
+    }
+  },
+
+  update_player: function(player, deltat) {
+    player.score += deltat;
+    if (Utility.getTimestamp() - player.reload_timestamp > RELOAD_INTERVAL) {
+      player.bullets += parseInt(player.score/RELOAD_INTERVAL, 10);
+      player.reload_timestamp = Utility.getTimestamp();
+    }
+    this.scoreboard.updateScoreboard(player);
+    //console.log(this.scoreboard.scores);
+    if (player.message != null) {
+      if (Utility.getTimestamp() - player.message_timestamp > MESSAGE_DECAY) {
+        var playerToUpdate = this.players.get(player.id);
+        playerToUpdate.message = null;
+      }
+    }
+  },
+
+  update_all_bullets: function(deltat) {
+    for(i=this.bullets.length-1; i >= 0; i--) {
+      this.update_bullet(this.bullets[i], deltat);
+      if (this.bullets[i].x < 0 || this.bullets[i].x > CANVAS_WIDTH || this.bullets[i].y < 0 || this.bullets[i].y > CANVAS_HEIGHT) {
+        this.bullets.splice(i, 1);
+      }
+    }
+    if (this.bullets.length > 0) {
+      io.sockets.emit('bullets', this.bullets);
+    }
+  },
+
+  update_bullet: function(bullet, deltat) {
+    switch (bullet.direction) {
+      case 0: //down
+        bullet.y = bullet.y + bullet.speed*deltat;
+        break;
+      case 1: //right
+        bullet.x = bullet.x + bullet.speed*deltat;
+        break;
+      case 2: //up
+        bullet.y = bullet.y - bullet.speed*deltat;
+        break;
+      case 3: //left
+        bullet.x = bullet.x - bullet.speed*deltat;
+        break;
+    }
+  },
+
+  checkBulletCollisions: function() {
+    for(i=this.bullets.length-1; i >= 0; i--) {
+      this.players.forEach( function(p) {
+        var b = this.bullets[i];
+        if (b != null) {
+          if (p.id != b.shot_by) { 
+            if (p.posx < b.x + BULLET_DIMENSION &&
+                p.posx + PLAYER_DIMENSION > b.x &&
+                p.posy + (2*PLAYER_DIMENSION/3) < b.y + BULLET_DIMENSION &&
+                p.posy + PLAYER_DIMENSION > b.y) {
+                var killer = this.players.get(b.shot_by);
+                killer.score+=p.score;
+                this.bullets.splice(i, 1);
+                this.players.delete(p.id);
+                console.log("player " + p.id + " shot by " + killer.id);
+                io.sockets.emit('state', Array.from(this.players.values()));
+                io.sockets.emit('player killed', p.id);
+                io.sockets.emit('bullets', this.bullets);
+                return;
+            }
+          }
+        }
+      }.bind(this));
+    }
+  },
 }
 
 var lounge = Lounge.create();
@@ -154,9 +346,11 @@ io.on('connection', function(socket){
   }
   socket.on('new player', function(newPlayer) {
     newPlayer.id = socket.id;
+    newPlayer.reload_timestamp = Utility.getTimestamp();
     lounge.players.set(newPlayer.id, newPlayer);
     console.log("new player: " + newPlayer.id);
     console.log("total players: " + lounge.players.size);
+    io.sockets.emit('scoreboard update', lounge.scoreboard.scores);
     update_occurred = true;
   });
   socket.on('update player', function(keyArray) {
@@ -204,11 +398,30 @@ io.on('connection', function(socket){
     player.holding_gun = holding_gun;
     update_occurred = true;
   });
+  socket.on('shot gun', function() {
+    var player = lounge.players.get(socket.id);
+    if (player.holding_gun && player.bullets > 0 && player.username != "anonymous" && player.username != null && player.username != "") {
+      player.last_shot = Utility.getTimestamp();
+      player.bullets--;
+      player.score-=RELOAD_INTERVAL;
+      var newBullet = Bullet.create(player.posx + PLAYER_DIMENSION/2, player.posy + PLAYER_DIMENSION/2, player.direction);
+      newBullet.shot_by = player.id;
+      lounge.bullets.push(newBullet);
+    }
+  });
+  socket.on('secret reload', function() {
+    var player = lounge.players.get(socket.id);
+    player.bullets+=5;
+  });
   socket.on('holding beer', function(holding_beer) {
     var player = lounge.players.get(socket.id);
     console.log(player.id + " beer: " + holding_beer);
     player.holding_beer = holding_beer;
     update_occurred = true;
+  });
+  socket.on('bong hit', function(){
+    var player = lounge.players.get(socket.id);
+    io.sockets.emit('bong hit', SmokePuff.create(player.posx, player.posy - PLAYER_DIMENSION/3));
   });
   socket.on('disconnect', function(newPlayer) {
     lounge.players.delete(socket.id);
@@ -217,20 +430,20 @@ io.on('connection', function(socket){
   });
 });
 
+var curr_timestamp = Utility.getTimestamp();
+var prev_timestamp;
 setInterval(function() {
-  var players = Array.from(lounge.players.values());
-  if (update_occurred) {
-    io.sockets.emit('state', players);
-    update_occurred = false;
+  prev_timestamp = curr_timestamp;
+  curr_timestamp = Utility.getTimestamp();
+
+  var deltat = curr_timestamp - prev_timestamp;
+  lounge.checkBulletCollisions();
+  lounge.update_all_players(deltat);
+  lounge.update_all_bullets(deltat);
+
+  if (lounge.scoreboard.updated) {
+    io.sockets.emit('scoreboard update', lounge.scoreboard.scores);
   }
-  players.forEach(function(player) {
-    if (player.message != null) {
-      if (Utility.getTimestamp() - player.message_timestamp > MESSAGE_DECAY) {
-        var playerToUpdate = lounge.players.get(player.id);
-        playerToUpdate.message = null;
-      }
-    }
-  });
 }, 1000/60);
 
 module.exports = socketApi;
